4c2d45bef4379e9f6de85cd56ea6f3cb09336499
duongtiendat1990 <48908567+duongtiendat1990@users.noreply.github.com>
2019-03-27 11:42:30 +0700

    Bài 3

---
+++ script.js
@@ -0,0 +1,4 @@
+let R = parseInt(prompt("Input Radius Length ",));
+let S = R * R * Math.PI
+document.write("Radius Length is " + R); document.write('</br>');
+document.write("Circle Area is " + S);
